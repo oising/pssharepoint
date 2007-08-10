f2260de77891e145dc61e52290363138c0209669
oisin_cp <SND\oisin_cp>
2007-08-10 02:18:38 +0000

    some initial cross-provider support classes.

---
+++ Trunk/Src/BackingStoreProviderBase/GlobalSuppressions.cs
@@ -0,0 +1,1 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2116:AptcaMethodsShouldOnlyCallAptcaMethods", Scope = "member", Target = "Nivot.PowerShell.StoreItem`1.GetParameterValue(System.String):TParam")]
