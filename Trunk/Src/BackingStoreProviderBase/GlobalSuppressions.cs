dbde29227a0662df2761a5d50dcb32fb0b317440
oisin_cp <SND\oisin_cp>
2009-02-11 02:59:42 +0000

    removed old backing store provider base source, (obsolete)

--- Trunk/Src/BackingStoreProviderBase/GlobalSuppressions.cs
+++
@@ -1,1 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA2116:AptcaMethodsShouldOnlyCallAptcaMethods", Scope = "member", Target = "Nivot.PowerShell.StoreItem`1.GetParameterValue(System.String):TParam")]
